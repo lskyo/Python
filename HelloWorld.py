25a4030b0e2ccaf4c3d73ee4d33af21bb21d5991
unknown <609911762@qq.com>
2018-05-22 22:54:04 +0800

    HelloWorld

---
+++ HelloWorld.py
@@ -0,0 +1,24 @@
+print('hello, world')
+print('I am', 'lskyo')
+print("100 + 1 = ", 100+1)
+# name = input("Please input your name:")
+# print('Hello!' + name)
+A = 100
+a = 101
+print('A == a :', A == a )
+print("Are you \"OK\"?")
+print('I\' am "ok"')
+print('\\\t\\')
+print('''b
+o
+o
+m''')
+# c = int(input("please input c(int):"))
+# print(type(c))
+# if c > 100 :
+# 	print("c > 100")
+# else:
+# 	print("c <= 100")
+print('10 / 3 =', 10/3)
+print('10 // 3 =', 10 // 3)
+print('10 % 3 =', 10%3)
