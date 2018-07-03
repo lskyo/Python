23c996f4768bce3b95c1d7e0c623461fd425bca8
lskyo <609911762@qq.com>
2018-07-03 17:04:58 +0800

    异常捕捉和单元测试

---
+++ try-catch.py
@@ -0,0 +1,67 @@
+import logging
+
+try:
+    print('try...')
+    r = 10 / int('2')
+    print('result:', r)
+except ValueError as e:
+    print('ValueError:', e)
+except ZeroDivisionError as e:
+    print('ZeroDivisionError:', e)
+else:
+    print('no error!')
+finally:
+    print('finally...')
+print('END')
+
+
+def foo(s):
+    return 10 / int(s)
+
+
+def bar(s):
+    return foo(s) * 2
+
+
+try:
+    bar('0')
+except Exception as e:
+    print('Error:', e)
+finally:
+    print('finally...')
+
+
+def foo(s):
+    return 10 / int(s)
+
+
+def bar(s):
+    return foo(s) * 2
+
+
+def main():
+    try:
+        bar('0')
+    except Exception as e:
+        logging.exception(e)
+
+
+main()
+
+
+class FooError(ValueError):
+    pass
+
+
+def foo(s):
+    n = int(s)
+    if n == 0:
+        raise FooError('invalid value: %s' % s)
+    return 10 / n
+
+
+try:
+    foo('0')
+except FooError:
+    print('FooError')
+
